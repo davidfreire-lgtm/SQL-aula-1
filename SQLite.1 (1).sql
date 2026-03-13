318fb301f3ddde3ab6407cf7e256402872951a32
davidfreire-lgtm <david.freire@escola.pr.gov.br>
2026-03-13 08:42:27 -0300

    Add files via upload

---
+++ SQLite.1 (1).sql
@@ -0,0 +1,9 @@
+INSERT into Hortifruti (produto, preço, comentario)
+VALUES
+('abacaxi', 10.20, 'Abacaxi alemão graudo'),
+('banana', 7.60, 'Sem casaca e fatiada'),
+('maçã', 11.00, 'quadrada'),
+('melão', 1.20,''),
+('kiwi', 4.99,'');
+
+SELECT produto, preço, comentario FROM Hortifruti;
---
+++ SQLite.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Hortifruti(
+id integer PRIMARY key AUTOINCREMENT,
+  produto text,
+  preço float,
+  comentario text
+);
+
+SELECT* from Hortifruti
